01d85c1f9e972ef2bb08a84c8376178ac9ab535e
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2017-04-01 18:21:37 +0200

    Removed some tabs from chapter 4

--- 04/4_hello_recursion.hs
+++ 04/4_hello_recursion.hs
@@ -35,6 +35,6 @@ elem' a (x:xs)
 quicksort :: Ord a => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) =
-	let smallerOrEqual = [a | a <- xs, a <= x]
-	    larger = [a | a <- xs, a > x]
-	in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      larger = [a | a <- xs, a > x]
+  in quicksort smallerOrEqual ++ [x] ++ quicksort larger
